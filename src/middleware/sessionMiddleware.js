27103cf91d3020222640d04c4fc85048ee2a2dda
Nguyễn Duy Anh <69722842+duyanhng01@users.noreply.github.com>
2024-06-27 07:35:30 +0700

    anhdnd

--- src/middleware/sessionMiddleware.js
+++ src/middleware/sessionMiddleware.js
@@ -1,14 +1,25 @@
 
+// const checkSession = (req, res, next) => {
+//     if (req.session && req.session.userData) {
+//         res.locals.user = req.session.userData;
+//         next(); 
+//     } else {
+//         if (req.path === '/' || req.path === '/login')  {
+//             next(); 
+//         } else {
+//             return res.status(403).redirect('/login');
+//         }
+//     }
+// };
+
+// module.exports = checkSession;
 const checkSession = (req, res, next) => {
     if (req.session && req.session.userData) {
         res.locals.user = req.session.userData;
+        console.log(req.session.userData); 
         next(); 
     } else {
-        if (req.path === '/' || req.path === '/login') {
-            next(); 
-        } else {
-            return res.status(403).redirect('/login');
-        }
+        res.redirect('/'); 
     }
 };
 
